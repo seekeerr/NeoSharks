0a317735f832ea54511789a0e8bca99b48361cc8
Uttam Singh <Uttam.Singh@ainetix.com>
2025-09-10 14:11:53 +0530

    test

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,3 +1,4 @@
+// app/api/contact/route.js
 import { NextResponse } from "next/server";
 
 const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzzF8vCwotO7Qq9Ih_i7RcEi20ALb1I4L62Pr8f2s0X9OIMQTaP36mFYtaspDR1UgbAXg/exec";
